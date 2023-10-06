d01ef01a8fc8d6a2df769b99acef3a82e8b6a834
David Pouani <davidpouani228@gmail.com>
2023-10-07 01:54:53 +0200

    configuration docker/propeties file

---
+++ src/main/java/com/relocation/api/RelocationAppApplication.java
@@ -0,0 +1,13 @@
+package com.relocation.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelocationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelocationAppApplication.class, args);
+	}
+
+}
